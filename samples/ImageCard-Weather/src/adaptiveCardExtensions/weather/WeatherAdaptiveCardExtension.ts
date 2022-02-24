c1c5c11ce8fff4ece9b6e89b1e8b2db341101db8
Aakash Bhardwaj <aabhardwaj@microsoft.com>
2022-02-24 12:19:33 +0530

    Added New Sample: Weather ACE

---
+++ samples/ImageCard-Weather/src/adaptiveCardExtensions/weather/WeatherAdaptiveCardExtension.ts
@@ -0,0 +1,126 @@
+import { IPropertyPaneConfiguration } from '@microsoft/sp-property-pane';
+import { BaseAdaptiveCardExtension, CardSize } from '@microsoft/sp-adaptive-card-extension-base';
+import { CardView } from './cardView/CardView';
+import { QuickView } from './quickView/QuickView';
+import { WeatherPropertyPane } from './WeatherPropertyPane';
+import { SetupCardView } from './cardView/SetupCardView';
+import { ErrorCardView } from './cardView/ErrorCardView';
+import { WeatherService } from '../../services/WeatherService';
+import { IWeatherAdaptiveCardExtensionState } from './IWeatherAdaptiveCardExtensionState';
+
+export interface IWeatherAdaptiveCardExtensionProps {
+  title: string;
+  imageUrl: string;
+  bingMapsKey: string;
+  azureMapsKey: string;
+  searchValue: string;
+  selectedLocation: string;
+}
+
+const CARD_VIEW_REGISTRY_ID: string = 'Weather_CARD_VIEW';
+const SETUP_CARD_VIEW_REGISTRY_ID: string = 'Weather_SETUP_CARD_VIEW';
+const ERROR_CARD_VIEW_REGISTRY_ID: string = 'Weather_ERROR_CARD_VIEW';
+export const QUICK_VIEW_REGISTRY_ID: string = 'Weather_QUICK_VIEW';
+
+export default class WeatherAdaptiveCardExtension extends BaseAdaptiveCardExtension<
+  IWeatherAdaptiveCardExtensionProps,
+  IWeatherAdaptiveCardExtensionState
+> {
+  private _deferredPropertyPane: WeatherPropertyPane | undefined;
+  private weatherService: WeatherService;
+
+  public async onInit(): Promise<void> {
+    this.state = {
+      temperature: undefined,
+      wind: undefined,
+      visibility: undefined,
+      pressure: undefined,
+      cloudCover: 0,
+      iconCode: 0,
+      dateTime: '',
+      locationName: '',
+      phrase: '',
+      airQuality: undefined,
+      asthmaForecast: undefined,
+      fluForecast: undefined,
+      dustForecast: undefined,
+      cardSize: this.cardSize
+    };
+
+    this.weatherService = new WeatherService(this.context);
+    this.cardNavigator.register(CARD_VIEW_REGISTRY_ID, () => new CardView());
+    this.cardNavigator.register(SETUP_CARD_VIEW_REGISTRY_ID, () => new SetupCardView());
+    this.cardNavigator.register(ERROR_CARD_VIEW_REGISTRY_ID, () => new ErrorCardView());
+    this.quickViewNavigator.register(QUICK_VIEW_REGISTRY_ID, () => new QuickView());
+    await this.getWeather();
+    return Promise.resolve();
+  }
+
+  private async getWeather() {
+    if (!this.properties.azureMapsKey || !this.properties.bingMapsKey || !this.properties.selectedLocation) {
+      this.cardNavigator.replace(SETUP_CARD_VIEW_REGISTRY_ID);
+    } else {
+      try {
+        const locationName = this.properties.selectedLocation.split(';')[0];
+        const latitude = this.properties.selectedLocation.split(';')[1];
+        const longitude = this.properties.selectedLocation.split(';')[2];
+
+        const weatherResult = await this.weatherService.GetWeatherResponse(latitude, longitude, this.properties.azureMapsKey);
+        const airQuality = await this.weatherService.GetAirQuality(latitude, longitude, this.properties.azureMapsKey);
+        const healthConditions = await this.weatherService.GetDailyIndices(latitude, longitude, 10, this.properties.azureMapsKey);
+        console.log(healthConditions);
+        if (!weatherResult) {
+          this.cardNavigator.replace(ERROR_CARD_VIEW_REGISTRY_ID);
+          return;
+        }
+        this.setState({
+          temperature: weatherResult.temperature,
+          wind: weatherResult.wind,
+          visibility: weatherResult.visibility,
+          pressure: weatherResult.pressure,
+          iconCode: weatherResult.iconCode,
+          cloudCover: weatherResult.cloudCover,
+          dateTime: weatherResult.dateTime,
+          locationName: locationName,
+          airQuality: airQuality,
+          asthmaForecast: healthConditions.filter(x => x.indexId === 23)[0],
+          fluForecast: healthConditions.filter(x => x.indexId === 26)[0],
+          dustForecast: healthConditions.filter(x => x.indexId === 18)[0],
+          phrase: weatherResult.phrase
+        });
+        this.cardNavigator.replace(CARD_VIEW_REGISTRY_ID);
+      } catch (error) {
+        console.log(error);
+        this.cardNavigator.replace(ERROR_CARD_VIEW_REGISTRY_ID);
+      }
+    }
+  }
+
+  protected loadPropertyPaneResources(): Promise<void> {
+    return import(
+      /* webpackChunkName: 'Weather-property-pane'*/
+      './WeatherPropertyPane'
+    )
+      .then(
+        (component) => {
+          this._deferredPropertyPane = new component.WeatherPropertyPane(this.properties, this.context, this.weatherService);
+        }
+      );
+  }
+
+  protected async onPropertyPaneFieldChanged(propertyPath: string, oldValue: string, newValue: string) {
+    super.onPropertyPaneFieldChanged(propertyPath, oldValue, newValue);
+    if ((propertyPath === 'bingMapsKey') || (propertyPath === 'azureMapsKey') || (propertyPath === 'selectedLocation')) {
+      await this.getWeather();
+    }
+    this.context.propertyPane.refresh();
+  }
+
+  protected renderCard(): string | undefined {
+    return CARD_VIEW_REGISTRY_ID;
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return this._deferredPropertyPane!.getPropertyPaneConfiguration();
+  }
+}
---
+++ samples/ImageCard-Weather/src/adaptiveCardExtensions/weather/loc/en-us.js
@@ -0,0 +1,21 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Write 1-3 sentences describing the functionality of this component.",
+    "TitleFieldLabel": "Card title",
+    "Title": "Adaptive Card Extension",
+    "SubTitle": "Quick view",
+    "PrimaryText": "SPFx Adaptive Card Extension",
+    "Description": "Create your SPFx Adaptive Card Extension solution!",
+    "QuickViewButton": "Get Details",
+    "ErrorCardPrimaryTextLabel": "Some Error Occurred",
+    "ErrorCardDescriptionLabel": "Please check the Bing Maps and Azure Maps API keys in the property pane are correct.",
+    "SetupCardPrimaryTextLabel": "Setup Incomplete",
+    "SetupCardDescriptionLabel": "You must set the Bing Maps and Azure Maps API keys from the property pane and select a location.",
+    "AzureMapsKeyLabel": "Azure Maps Key",
+    "BingMapsKeyLabel": "Bing Maps Key",
+    "LocationLabel": "Location",
+    "SearchLocationPlaceholder": "Search location",
+    "SearchLocationNoBingPlaceholder": "Set Bing Maps Key to search location",
+    "ImageURLLabel" : "Image URL"
+  }
+});
