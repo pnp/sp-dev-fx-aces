99f7963eb339ad691e593e5dfacc66c0571d9642
Derek <derekcp@sympraxisconsulting.com>
2022-06-03 10:21:25 +0000

    approving Recent Files ACE.

---
+++ samples/PrimaryTextCard-WordOfTheDay/lib/adaptiveCardExtensions/wordOfTheDay/model/WordOfTheDaySample.d.ts
@@ -0,0 +1,5 @@
+import { WordOfTheDay } from "./WordOfTheDay";
+export default abstract class WordOfTheDaySampleData {
+    static WordOfTheDaySample: () => WordOfTheDay;
+}
+//# sourceMappingURL=WordOfTheDaySample.d.ts.map
