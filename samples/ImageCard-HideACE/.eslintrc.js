398b17fe6cc401a56c95c165ae5e892b1f863dff
Derek <derekcp@sympraxisconsulting.com>
2022-11-14 11:25:44 +0000

    upgraded project to SPFx 1.15.2

---
+++ samples/ImageCard-HideACE/.eslintrc.js
@@ -0,0 +1,8 @@
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+module.exports = {
+  extends: ['@microsoft/eslint-config-spfx/lib/profiles/default'],
+  parserOptions: { tsconfigRootDir: __dirname },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": 0,
+  }
+};
