a471e494ac440e763c35fc961308161c83f49d81
Yves Habersaat <yveshabersaat@hotmail.com>
2022-12-21 15:58:48 +0100

    Updated sample BasicCard-StatusMessage

--- samples/BasicCard-StatusMessage/src/adaptiveCardExtensions/statusMessage/loc/en-us.js
+++ samples/BasicCard-StatusMessage/src/adaptiveCardExtensions/statusMessage/loc/en-us.js
@@ -1,6 +1,6 @@
 define([], function () {
   return {
-    "PropertyPaneDescription": "This ACE demonstrates how to use the setStatusMessage Graph API endpoint (beta) to set and retrieve a presence status message for a user.",
+    "PropertyPaneDescription": "This ACE demonstrates how to use the Presence Graph API endpoints (beta) to set and retrieve a presence status message for a user.",
     "QuickViewButton": "Set status",
     "NoStatusMessageText": "You have no status message.",
     "SetStatusMessageTitle": "Set status message"
