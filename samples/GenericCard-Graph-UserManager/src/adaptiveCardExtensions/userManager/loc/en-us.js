1606cefaa1e97b265f0f43b03b972e693f4be265
guidozam <guido@piasys.it>
2024-03-09 15:55:36 +0100

    Added GenericCard-Graph-UserManager

---
+++ samples/GenericCard-Graph-UserManager/src/adaptiveCardExtensions/userManager/loc/en-us.js
@@ -0,0 +1,8 @@
+define([], function() {
+  return {
+    "CardTitle": "My manager",
+    "CardViewButton": "Open chat",
+    "Error": "Ooops!",
+    "ReloadButton": "Reload"
+  }
+});
---
+++ samples/GenericCard-Graph-UserManager/src/adaptiveCardExtensions/userManager/loc/mystring.d.ts
@@ -0,0 +1,11 @@
+declare interface IUserManagerAdaptiveCardExtensionStrings {
+	CardTitle: string;
+	CardViewButton: string;
+	Error: string;
+	ReloadButton: string;
+}
+
+declare module 'UserManagerAdaptiveCardExtensionStrings' {
+  const strings: IUserManagerAdaptiveCardExtensionStrings;
+  export = strings;
+}
