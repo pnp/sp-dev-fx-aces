0b6aefe51de61596d6be9a20c861512c65ef9711
Ahmad Jad Alhak <ahmad.jad86@gmail.com>
2024-10-28 20:25:23 +0100

    Add new sample BasicCard-HTML-React-QuickView

---
+++ BasicCard-HTML-React-QuickView/src/adaptiveCardExtensions/reactQuickView/ReactQuickViewPropertyPane.ts
@@ -0,0 +1,26 @@
+import { IPropertyPaneConfiguration, PropertyPaneTextField } from '@microsoft/sp-property-pane';
+import * as strings from 'ReactQuickViewAdaptiveCardExtensionStrings';
+
+export class ReactQuickViewPropertyPane {
+  public getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: { description: strings.PropertyPaneDescription },
+          groups: [
+            {
+              groupFields: [
+                PropertyPaneTextField('title', {
+                  label: strings.TitleFieldLabel
+                }),
+                PropertyPaneTextField('listName', {
+                  label: strings.ListNameFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ BasicCard-HTML-React-QuickView/src/adaptiveCardExtensions/reactQuickView/loc/mystring.d.ts
@@ -0,0 +1,15 @@
+declare interface IReactQuickViewAdaptiveCardExtensionStrings {
+  PropertyPaneDescription: string;
+  TitleFieldLabel: string;
+  ListNameFieldLabel: string;
+  Title: string;
+  SubTitle: string;
+  PrimaryText: string;
+  Description: string;
+  QuickViewButton: string;
+}
+
+declare module 'ReactQuickViewAdaptiveCardExtensionStrings' {
+  const strings: IReactQuickViewAdaptiveCardExtensionStrings;
+  export = strings;
+}
