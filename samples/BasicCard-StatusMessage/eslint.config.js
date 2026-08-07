16f8bd17737eca69b676b75439a27ad450bd105d
Aimery Thomas <7100077+a1mery@users.noreply.github.com>
2026-08-08 00:25:32 +0200

    upgrade to SPFx 1.23

---
+++ samples/BasicCard-StatusMessage/eslint.config.js
@@ -0,0 +1,21 @@
+const spfxProfile = require('@microsoft/eslint-config-spfx/lib/flat-profiles/react');
+
+module.exports = [
+  ...spfxProfile,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json'
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn'
+    }
+  }
+];
